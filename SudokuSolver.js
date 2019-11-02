e171f13c3e4903961a939b5442bff2f62f168bca
soninlaw <jonsoninlaw@gmail.com>
2019-11-02 15:01:18 +0100

    Résolution des lignes

---
+++ SudokuSolver.js
@@ -0,0 +1,156 @@
+var puzzle = [
+    [5,3,0,0,7,0,0,0,0],
+    [6,0,0,1,9,5,0,0,0],
+    [0,9,8,0,0,0,0,6,0],
+    [8,0,0,0,6,0,0,0,3],
+    [4,0,0,8,0,3,0,0,1],
+    [7,0,0,0,2,0,0,0,6],
+    [0,6,0,0,0,0,2,8,0],
+    [0,0,0,4,1,9,6,0,5],
+    [0,0,0,0,8,0,0,7,9]];
+
+sudokuSolver(puzzle);
+
+function sudokuSolver(puzzle) {
+
+    let numbersCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    let emptySpace = [[9, 9, 9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9, 9, 9, 9, 9, 9]];
+    let empty = 81;
+
+    // Get count of all numbers and empty spaces
+    let boxCount = 0;
+    for (let row = 0; row < 9; row++) {
+        if (row > 0 && row % 3 === 0) {boxCount += 3};
+        for (let col = 0; col < 9; col++) {
+            if (puzzle[row][col] != 0) {
+                numbersCount[puzzle[row][col] - 1]++;
+                emptySpace[0][row]--;
+                emptySpace[1][col]--;
+                emptySpace[2][boxCount + Math.trunc(col / 3)]--;
+                empty--;
+            }
+        }
+    }
+
+    //while (empty > 0) {
+
+        // Check which number is most present
+        let max = 0;
+        let maxNumber = 0;
+        for (let i = 0; i < 9; i++) {
+            if (numbersCount[i] > max) {
+                max = numbersCount[i];
+                maxNumber = i + 1;
+            }
+        }
+        boxCount = 0;
+        let isInRow = false;
+        let isInCol = false;
+        let isInBox = false;
+        let numberCheck = false;
+        let colCheck = 0;
+        let rowCheck = 0;
+        let cols = new Array();
+        // Check if chosen number is in the row
+
+        // Vérification ligne par ligne
+        for (let row = 0; row < 9; row++) {
+
+            // On initialise les booléens à faux
+            isInRow = false;
+            isInCol = false;
+            numberCheck === false;
+
+            if (row > 0 && row % 3 === 0) {boxCount += 3};
+            
+            // On parcourt la ligne entière pour vérifier que le chiffre n'est pas présent
+            for (let col = 0; col < 9; col++) {
+
+                // Si il est présent
+                if (!isInRow && puzzle[row][col] === maxNumber) {
+                    isInRow = true;
+
+                    // On ajoute le numéro de la colonne à la liste
+                    if (!cols.includes(col)) {
+                        cols.push(col);
+                    }
+                    break;
+                }
+            }
+
+            // Si il n'est pas présent
+            if (!isInRow) {
+
+                // On parcourt la ligne colone par colonne
+                for (let testCol = 0; testCol < 9; testCol++) {
+                    isInCol = false;
+
+                    // Si la case est vide et que le nombre n'existe pas déjà dans la même colonne
+                    if (puzzle[row][testCol] === 0 && !cols.includes(testCol)) {
+
+                        // On parcourt toutes les cases sur la même colonne
+                        for (let testRow = 0; testRow < 9; testRow++) {
+
+                            // Si le nombre est présent dans la case ou a déjà été repéré
+                            if (!isInCol && puzzle[testRow][testCol] === maxNumber) {
+
+                                // On indique qu'il est présent
+                                isInCol = true;
+
+                                // On ajoute la colonne à la liste
+                                cols.push(testCol);
+                                break;
+                            }
+                        }
+
+                        // Si le chiffre n'est pas présent sur la colonne
+                        if (!isInCol) {
+
+                            // Si il a déjà été validé
+                            if (numberCheck) {
+                            
+                                // On indique qu'il n'est plus validé
+                                numberCheck = false;
+
+                                // Et on arrête de chercher
+                                console.log("break");
+                                break;
+                            }
+
+                            // Sinon
+                            else {
+
+                                // On valide le chiffre
+                                numberCheck = true;
+    
+                                // Et on indique dans quelle case l'insérer
+                                colCheck = testCol;
+                                console.log("validé");
+                            }
+                        }
+                    }
+                }
+
+                // A la fin du parcours, si le chiffre est validé, on l'insère dans la case
+                if (numberCheck) {
+                    puzzle[row][colCheck] = maxNumber;
+                    numbersCount[maxNumber - 1]++;
+                    emptySpace[0][row]--;
+                    emptySpace[1][colCheck]--;
+                    emptySpace[2][boxCount + Math.trunc(colCheck / 3)]--;
+                    empty--;
+                    
+                }
+            }
+        }
+    //}
+
+    //console.log("numbersCount : " + numbersCount);
+    //console.log(emptySpace[0]);
+    //console.log(emptySpace[1]);
+    //console.log(emptySpace[2]);
+    console.log("empty spaces : " + empty);
+    //console.log("maxNumber : " + maxNumber);
+    console.log("cols filled : " + cols);
+
+}
